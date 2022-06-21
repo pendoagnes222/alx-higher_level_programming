098fd689db9931786415b2ffea5a1114adc3414a
dianakyalo202 <dianakyalo202@gmai.com>
2022-06-21 20:38:25 +0300

    OOP

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Creates a square class"""
+
+
+class Square():
+    """
+    Define a square
+    """
+    def __init__(self, size):
+        """
+        size (int): first parameter
+        """
+        self.__size = size
