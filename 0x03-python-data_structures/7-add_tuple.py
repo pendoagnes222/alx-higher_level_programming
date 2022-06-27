6cad00a49a3b8d3ad9708ad2617494cb55a46e70
dianakyalo202 <dianakyalo202@gmai.com>
2022-06-27 10:14:17 +0300

    Commit

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-""" adds two tuples
-"""
-
-
 def add_tuple(tuple_a=(), tuple_b=()):
-    """ this function adds 2 tuples"""
-    if len(tuple_a) > 2:
-        tuple_a = (tuple_a[0], tuple_a[1])
-    if len(tuple_b) > 2:
-        tuple_b = (tuple_b[0], tuple_b[1])
-    if len(tuple_a) == 1:
-        tuple_a = (tuple_a[0], 0)
-    if len(tuple_b) == 1:
-        tuple_b = (tuple_b[0], 0)
-    if len(tuple_a) == 0:
-        tuple_a = (0, 0)
-    if len(tuple_b) == 0:
-        tuple_b = (0, 0)
-    sum_tup = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    return 
-sum_tup
+    if tuple_a or tuple_b:
+        results = []
+        if len(tuple_a) == 0:
+            tuple_a = tuple_a[:2]
+            return tuple_b
+        if len(tuple_b) == 0:
+            tuple_b = tuple_b[:2]
+            return tuple_a
+        if len(tuple_a) == 1:
+            tuple_a = (tuple_a[0], 0)
+        if len(tuple_b) == 1:
+            tuple_b = (tuple_b[0], 0)
+        tuple_a = tuple_a[:2]
+        tuple_b = tuple_b[:2]
+        for x, y in zip(tuple_a, tuple_b):
+            zipped = x+y
+            results.append(zipped)
+        results = tuple(results)
+        return results
+    return
