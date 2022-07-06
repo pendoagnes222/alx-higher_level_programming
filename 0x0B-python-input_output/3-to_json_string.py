5b1fa3fe3f2c8181de0f8538d8e354068fcbd654
dianakyalo202 <dianakyalo202@gmai.com>
2022-07-06 05:05:50 +0300

    input/output

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module has a function that returns the JSON representation of
+an object
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """ Returns the JSON representation of
+    an object"""
+    return json.dumps(my_obj)
