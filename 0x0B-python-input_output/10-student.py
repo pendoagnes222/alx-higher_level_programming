28d081d769586b603e4190a734f31cadbab53128
dianakyalo202 <dianakyalo202@gmai.com>
2022-07-06 05:18:18 +0300

    input/output

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+A module containing a class
+"""
+
+
+class Student():
+    """
+    A Student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Instantiation of class attributes
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves dictionary representation of student"""
+        if type(attrs) is list and all(type(x) is str for x in attrs):
+            dictionary = {}
+            for i in attrs:
+                if i in self.__dict__:
+                    dictionary[i] = self.__dict__[i]
+            return dictionary
+        else:
+            return self.__dict__
