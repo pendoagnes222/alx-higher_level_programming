f2fb24316080bda0e335a6d8595e310c9258f0e0
MY tutorial <99383447+dianakyalo202@users.noreply.github.com>
2022-07-21 05:52:56 +0300

    Add files via upload

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    div = 0
+
     try:
-        div = a / b
-        return div
-    except:
-        div = None
-        return None
+        response = a / b
+    except ZeroDivisionError:
+        response = None
     finally:
-        print("Inside result: {}".format(div))
+        print('Inside result: {}'.format(response))
+
+    return response
